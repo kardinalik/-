08536d645745bcaab48857697da9fe7b0c527edd
kardinalik <pvt711@mail.ru>
2024-09-04 23:26:34 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+homemade_count = 12
+hours_spent = 1.5
+name_of_course = 'Python'
+print ('Курс:', name_of_course , ',всего задач:' , homemade_count, ',затрачено часов:', hours_spent,
+       ',среднее время выполнения:' , hours_spent/homemade_count , 'часа')
